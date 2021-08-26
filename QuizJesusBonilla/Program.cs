52d1c12cd7d3627ac9f238839c8b1ae23f1acbbb
jesusbonillaupb <80067532+jesusbonillaupb@users.noreply.github.com>
2021-08-26 15:10:25 -0500

    Revert "prueba"
    This reverts commit 986f5cf6f8c4afa64e913c45061e9313848297b1.

--- QuizJesusBonilla/Program.cs
+++ QuizJesusBonilla/Program.cs
@@ -16,7 +16,7 @@ namespace QuizJesusBonilla
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Home());
+            Application.Run(new Form1());
         }
     }
 }
